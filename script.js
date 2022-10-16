870e6b31d05a441cd16e4da18bd6341475d5079f
Love <lovecynthiau@gmail.com>
2022-10-16 10:01:40 +0100

    Removed padStart

--- script.js
+++ script.js
@@ -1,52 +1,48 @@
-
-let startCount = document.getElementById('start');
-let stopCount = document.getElementById('stop');
-let resetCount = document.getElementById('reset');
+let startCount = document.getElementById("start");
+let stopCount = document.getElementById("stop");
+let resetCount = document.getElementById("reset");
 let seconds = 00;
 let tens = 00;
-let tensCount = document.getElementById('tens');
-let secondsCount = document.getElementById('seconds');
+let tensCount = document.getElementById("tens");
+let secondsCount = document.getElementById("seconds");
 let Interval;
 
-
 function increaseCount() {
-    seconds++;
-    if (seconds <= 9){
-        secondsCount.innerHTML = seconds;
-        secondsCount.innerHTML.padStart(2, '0');
-    }
-    if(seconds > 9){
-        secondsCount.innerHTML = seconds;
-    }
-    if (seconds > 99){
-        tens++;
-        tensCount.innerHTML = tens;
-        // tensCount.innerHTML.padStart(2, '0');
-        seconds = 0;
-        console.log("secondsCount => ", secondsCount);
-        secondsCount.innerHTML = 0;
-        secondsCount.innerHTML.padStart(2, '0');
-    }
-    if (tens < 9){
-        tensCount.innerHTML = tens;
-    }
+  seconds++;
+  if (seconds <= 9) {
+    secondsCount.innerHTML = "0" + seconds;
+  }
+
+  if (seconds > 9) {
+    secondsCount.innerHTML = seconds;
+  }
+
+  if (seconds > 99) {
+    tens++;
+    tensCount.innerHTML = "0" + tens;
+
+    seconds = 0;
+    secondsCount.innerHTML = "0" + 0;
+  }
+
+  if (tens > 9) {
+    tensCount.innerHTML = tens;
+  }
+}
+
+startCount.onclick = function () {
+  clearInterval(Interval);
+  Interval = setInterval(increaseCount, 10);
 };
 
-startCount.onclick = function(){
-    clearInterval(Interval);
-    Interval = setInterval(increaseCount, 10)
-}
+stopCount.onclick = function () {
+  clearInterval(Interval);
+};
 
-stopCount.onclick = function(){
-    clearInterval(Interval);
-}
-
-resetCount.onclick = function (){
-    clearInterval(Interval);
-    tens = '00';
-    seconds= '00';
-    tensCount.innerHTML = tens;
-    secondsCount.innerHTML = seconds;
-}
-
-
+resetCount.onclick = function () {
+  clearInterval(Interval);
+  tens = "00";
+  seconds = "00";
+  tensCount.innerHTML = tens;
+  secondsCount.innerHTML = seconds;
+};
